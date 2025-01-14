7e9a9ff6211d032e994a2975499f2ef0dd991bd2
İsmail Baran <ismailbaran04@gmail.com>
2025-01-14 15:05:00 +0300

    proje oluşturuldu entity ler tamamlandı service yazıldı ayarları yapıldı views ayarlanıyor

---
+++ MyMongoProjectNight/Services/ICustomerService.cs
@@ -0,0 +1,13 @@
+﻿using MyMongoProjectNight.Dtos.CustomerDtos;
+
+namespace MyMongoProjectNight.Services
+{
+    public interface ICustomerService
+    {
+        Task<List<ResultCustomerDto>> GetAllCustomerAsync();
+        Task CreateCustomerAsync(CreateCustomerDto createCustomerDto);
+        Task UpdateCustomerAsync(UpdateCustomerDto updateCustomerDto);
+        Task DeleteCustomerAsync(string customerId);
+        Task<GetByIdCustomerDto> GetByIdCustomerAsync(string customerId);
+    }
+}
